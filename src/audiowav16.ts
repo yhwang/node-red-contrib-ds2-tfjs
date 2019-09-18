1702c863110da6ca6cf98d555e9f70e9c1f6791f
Yihong Wang <yh.wang@ibm.com>
2019-09-17 18:00:56 -0700

    Create skeleton of a custom node
    Add js/html for a custom node-red node.
    Layout the folder structure and put basic
    files first.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

---
+++ src/audiowav16.ts
@@ -0,0 +1,56 @@
+const TARGET_SAMPLE_RATE = 16000;
+
+// Downsampling and low-pass filter:
+// Input audio is typically 44.1kHz or 48kHz, this downsamples it to 16kHz.
+// It uses a FIR (finite impulse response) Filter to remove
+// (or, at least attinuate) audio frequencies > ~8kHz because sampled audio
+// cannot accurately represent frequiencies greater than half of the sample
+// rate. (Human voice tops out at < 4kHz, so nothing important is lost for
+// transcription.)
+// See http://dsp.stackexchange.com/a/37475/26392 for a good explanation of
+// this code.
+const FILTER = [
+  -0.037935,
+  -0.00089024,
+  0.040173,
+  0.019989,
+  0.0047792,
+  -0.058675,
+  -0.056487,
+  -0.0040653,
+  0.14527,
+  0.26927,
+  0.33913,
+  0.26927,
+  0.14527,
+  -0.0040653,
+  -0.056487,
+  -0.058675,
+  0.0047792,
+  0.019989,
+  0.040173,
+  -0.00089024,
+  -0.037935
+];
+
+/**
+ * Downsamples WebAudio to 16 kHz.
+ * @param audioBuff Float32Array audio chunk
+ * @return 'audio/l16' chunk
+ */
+export const downsampleTo16K = (audioBuff: Float32Array, srcSampleRate: number) => {
+  const ratio = srcSampleRate / TARGET_SAMPLE_RATE;
+  const outLen =
+      Math.floor((audioBuff.length - FILTER.length) / ratio) + 1;
+  console.log(`audioBuff.length: ${audioBuff.length}, outLen: ${outLen}`);
+  const rev = new Float32Array(outLen);
+  for (let i = 0, end = audioBuff.length - FILTER.length + 1; i < end; i++) {
+    const offset = Math.round(ratio * i);
+    let sample = 0;
+    for (let j = 0, len = FILTER.length; j < len; ++j) {
+      sample += audioBuff[offset + j] * FILTER[j];
+    }
+    rev[i] = sample;
+  }
+  return rev;
+};
